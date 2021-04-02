d605d21fa17f1fda1e76c5789a1efd553eccd90c
Austn Hughes <austen.hughes.91@gmail.com>
2021-04-01 23:34:42 -0600

    models worked out

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      CHECK (defaultValue = DECIMAL(10, 2))
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      CHECK (defaultValue = INTEGER)
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+    },
+
     // define columns
   },
   {
